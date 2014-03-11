7ffae383a506bbb4ee6ae18bc769a8976f877909
Div <divya@heysparkbox.com>
2014-03-11 14:39:26 -0400

    added test for spares

--- bowling/tc_bowling.rb
+++ bowling/tc_bowling.rb
@@ -8,8 +8,16 @@ class Bowling < MiniTest::Unit::TestCase
     @bowl_on = Bowl.new
   end
 
-  def test_score
+  def test_simple_score
     assert_equal 20, @bowl_on.score("11111111111111111111")
+  end
+
+  def test_with_misses
+    assert_equal 90, @bowl_on.score("9-9-9-9-9-9-9-9-9-9-")
+  end
+
+  def test_with_second_spares
+    assert_equal 150, @bowl_on.score("5/5/5/5/5/5/5/5/5/5/5")
   end
 
 end
