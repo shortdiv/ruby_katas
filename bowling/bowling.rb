8f92999ba1fc4c7b15bc321026e99e1f7ffb6edc
Div <divya@heysparkbox.com>
2014-04-03 14:11:25 -0400

    Redid bowling kata

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,25 +1,12 @@
-class Bowling
+class Game
+  attr_reader :score
 
-  class OpenGame
+  def initialize
+    @score = 0
+  end
 
-    def what_kind_of_game?(game)
-      game.each_char do |throw|
-        if (throw == "/") || (throw == "X")
-          return false
-        else
-          return true
-        end
-      end
-    end
-
-    def score(game)
-      @points = 0
-      game.each_char do |single|
-        @points += single.to_i
-      end
-      return @points
-    end
-
+  def roll(pins)
+    @score += pins
   end
 
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -1,44 +1,32 @@
 require_relative '../bowling'
 
-describe Bowling do
-  before(:each) do
-    @bowl = Bowling::OpenGame.new
-  end
+describe Game do
 
-  context "when asked what kind of game it is" do
-    it "checks game and returns open game" do
-      results = @bowl.what_kind_of_game?("11111111111111111111")
-      expect(results).to eq(true)
+  it "has a zero score" do
+    expect(subject.score).to eq(0)
+  end
+  describe "Rolling a Ball" do
+    it "changes the score" do
+      subject.roll(4)
+      expect(subject.score).to eq(4)
     end
   end
 
-  context "when asked to calculate score given an open game with just ones" do
-    it "equates to 20" do
-      results = @bowl.score("11111111111111111111")
-      expect(results).to eq 20
-    end
-  end
-
-  context "when asked to calculate score with alternating 1s and misses where the first try is a score and second is a miss" do
-    it "equates to 90" do
-      results = @bowl.score("1-1-1-1-1-1-1-1-1-1-")
-      expect(results).to eq 10
-    end
-  end
-
-  context "when asked to calculate score with alternating 1s and misses where the first try is a miss and the second is a score" do
-    it "equates to 10" do
-      results = @bowl.score("-1-1-1-1-1-1-1-1-1-1")
-      expect(results).to eq 10
-    end
-  end
-
-  context "when asked to calculate score with one spare in the first frame" do
-    it "equates to 29" do
-      results = @bowl.score("3/111111111111111111")
-      expect(results).to eq 29
+  describe "Rolling 2 balls" do
+    it "adds two scores" do
+      subject.roll(4)
+      subject.roll(3)
+      expect(subject.score).to eq(7)
     end
   end
 
 end
 
+describe Frame
+  context "when fewer than 10 pins are knocked down" do
+    it "is an open frame" do
+      #when you call roll, what do you want code in toll to look like?
+    end
+  end
+
+end
